d9e7e6ac9dcb46e3844e8bc2c6f5ddd6ea069da1
Sebastián Estrella <sebas007estrella@gmail.com>
2014-11-25 15:41:53 -0500

    Add type alias Matrix

--- src/Snail.hs
+++ src/Snail.hs
@@ -1,23 +1,25 @@
 module Snail where
 
-snail :: [[Integer]] -> [Integer]
+type Matrix = [[Integer]]
+
+snail :: Matrix -> [Integer]
 snail []     = []
 snail matrix = borders matrix ++ (snail $ submatrix matrix)
 
-borders :: [[Integer]] -> [Integer]
+borders :: Matrix -> [Integer]
 borders matrix = topBorder matrix ++ rightBorder matrix ++ bottomBorder matrix ++ leftBorder matrix
 
-topBorder :: [[Integer]] -> [Integer]
+topBorder :: Matrix -> [Integer]
 topBorder matrix = head matrix
 
-rightBorder :: [[Integer]] -> [Integer]
+rightBorder :: Matrix -> [Integer]
 rightBorder matrix = map last $ tail matrix
 
-bottomBorder :: [[Integer]] -> [Integer]
+bottomBorder :: Matrix -> [Integer]
 bottomBorder matrix = reverse $ init $ last matrix
 
-leftBorder :: [[Integer]] -> [Integer]
+leftBorder :: Matrix -> [Integer]
 leftBorder matrix = reverse $ map head $ tail $ init matrix
 
-submatrix :: [[Integer]] -> [[Integer]]
+submatrix :: Matrix -> Matrix
 submatrix matrix = map (tail . init) $ tail $ init matrix
